8504691663fb32c0fe6b8e02a9cb70a23fbef1a9
Brennan Fife <brenfife@gmail.com>
2022-01-15 16:36:29 -0700

    -

--- src/pages/browse/[world].tsx
+++ src/pages/browse/[world].tsx
@@ -8,8 +8,10 @@ import {
   ModalContent,
   ModalBody,
   ModalCloseButton,
+  Box,
 } from '@chakra-ui/react'
 import SEO from '../../components/SEO'
+import Link from 'next/link'
 
 export default function BrowsePage({ world }) {
   const { prefetch, push, pathname } = useRouter()
@@ -21,6 +23,32 @@ export default function BrowsePage({ world }) {
   return (
     <>
       <SEO title={world} />
+      <Flex direction="column">
+        <div
+          style={{
+            display: 'inline-grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            gridGap: '10px',
+            gridAutoRows: 'minmax(100px, auto)',
+          }}
+        >
+          {data.map((id, index) => (
+            <Link key={index} href="/browse/[world]" as={`/browse/${id}`}>
+              <Flex
+                w="150px"
+                h="150px"
+                bgColor="lightblue"
+                as="a"
+                border="black solid 1px"
+                alignItems="center"
+                justifyContent="center"
+              >
+                {id}
+              </Flex>
+            </Link>
+          ))}
+        </div>
+      </Flex>
       <Modal isOpen={true} onClose={() => push('/')}>
         <ModalOverlay />
         <ModalContent>
